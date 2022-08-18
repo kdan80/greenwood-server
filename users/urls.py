0a2d9bb272d3758f38dab071adb13e2f2c50dc30
kdan80 <kierandansey80@gmail.com>
2022-08-18 18:49:32 +0100

    Set up routing/urls for users app

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+app_name = 'users'
+
+urlpatterns = [
+    #path('', registerView, name='register'),
+    #path('login/', loginView, name='login'),
+    #path('logout/', logoutView, name='logout'),
+    #path('register/', CreateCustomUserView.as_view(), name='create_user'),
+    #path('userinfo/', GetUserInfo.as_view(), name='get_user_info'),
+    #path('logout/blacklist/', BlacklistTokenView.as_view(), name='blacklist')
+]
